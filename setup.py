ad4910418bd61527d21266e4dbe284da785049bf
Zhernosek Andrei <zhernosek12@gmail.com>
2023-02-07 20:55:33 +0300

    v.08

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
 setup(
+    include_package_data=True,
     name='zhnftfermatwitter',
     version='0.0.6',
-    packages=["zhnftfermatwitter"],
+    packages=[],
+    install_requires=["requests", "selenium"],
     package_dir={'':"zhnftfermatwitter"},
     url='http://ferma.zhernosek.xyz/',
     license='',
